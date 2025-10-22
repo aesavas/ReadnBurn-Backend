b562d880cc3898189ae20b38af5b445257a4537f
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-22 13:45:09 +0100

    feat: accounts app urls added into main backend urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
 urlpatterns = [
+    path("api/", include("accounts.urls")),
     path("admin/", admin.site.urls),
 ]
