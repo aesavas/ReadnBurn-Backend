6621cc154738d324680245b9f0c4920798c2c19c
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-23 15:48:40 +0100

    feat: test settings created

---
+++ backend/backend/settings_test.py
@@ -0,0 +1,12 @@
+from .settings import BASE_DIR
+from .settings import *  # noqa: F401,F403
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR / "test_db.sqlite3"),
+    }
+}
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
+EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
