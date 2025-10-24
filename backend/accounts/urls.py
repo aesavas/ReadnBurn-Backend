6650402e0ec80921fd60abf77fe9bbc3e18e7c21
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-24 15:31:50 +0100

    feat: UserLoginView and its url created

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,6 +1,8 @@
+from accounts.views import UserLoginView
 from accounts.views import UserRegistrationView
 from django.urls import path
 
 urlpatterns = [
     path("auth/register/", UserRegistrationView.as_view(), name="user_register"),
+    path("auth/login/", UserLoginView.as_view(), name="user_login"),
 ]
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,6 +1,9 @@
+from accounts.serializers import UserLoginSerializer
 from accounts.serializers import UserRegistrationSerializer
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -30,4 +33,42 @@ class UserRegistrationView(APIView):
                 "errors": serializer.errors,
             },
             status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class UserLoginView(APIView):
+    """Handle user login requests."""
+
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request: Request) -> Response:
+        serializer = UserLoginSerializer(data=request.data)
+        try:
+            serializer.is_valid(raise_exception=True)
+        except ValidationError as e:
+            return Response(
+                {
+                    "status": "error",
+                    "message": "User login failed",
+                    "errors": {"detail": e.detail},
+                },
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        except AuthenticationFailed as e:
+            return Response(
+                {
+                    "status": "error",
+                    "message": "User login failed",
+                    "errors": {"detail": e.detail},
+                },
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        return Response(
+            {
+                "status": "success",
+                "message": "User logged in successfully",
+                "data": serializer.validated_data,
+            },
+            status=status.HTTP_200_OK,
         )
