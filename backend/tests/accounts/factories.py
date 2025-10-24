9162cf5387e4fd38a5ff58babf7182251ed9588b
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-24 15:32:53 +0100

    test: is_active field added factory and factory forced to use CustomManager

--- backend/tests/accounts/factories.py
+++ backend/tests/accounts/factories.py
@@ -1,4 +1,7 @@
 import uuid
+from typing import Any
+from typing import Type
+from typing import cast
 
 import factory
 from accounts.models import User
@@ -15,4 +18,16 @@ class UserFactory(DjangoModelFactory):
     last_name = factory.Faker("last_name")
     is_staff = False
     is_superuser = False
+    is_active = True
     email_verified = True
+    password = "default_password!"
+
+    @classmethod
+    def _create(cls, model_class: Type[User], *args: Any, **kwargs: Any) -> User:
+        """Override the default .create() to use our custom manager."""
+        manager = cls._get_manager(model_class)
+        # Pop the required arguments for our manager
+        email = kwargs.pop("email")
+        password = kwargs.pop("password")
+        # The remaining kwargs are the extra_fields
+        return cast(User, manager.create_user(email, password, **kwargs))
