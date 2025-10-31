0b97f234bd6d721eead322aeb347ba3e4daf9d81
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-31 22:44:25 +0000

    test: test cases created for SecretRetrieveView api

--- backend/tests/confidential/test_api.py
+++ backend/tests/confidential/test_api.py
@@ -1,12 +1,17 @@
+import uuid
 from datetime import datetime
 from datetime import timedelta
 from typing import cast
 
 import pytest
+from confidential.models import Secret
+from confidential.models import SecretViewLog
 from core.encryption import decrypt_message
 from django.utils import timezone
+from freezegun import freeze_time
 from pytest import MonkeyPatch
 from rest_framework.test import APIClient
+from tests.conftest import SecretFactoryCallable
 
 from backend.settings_test import TEST_ENCRYPTION_KEY
 
@@ -144,3 +149,125 @@ def test_secret_create_api_unauthorized(
     response = api_client.post("/api/secrets/create/", payload)
 
     assert response.status_code == 401
+
+
+@pytest.mark.django_db
+def test_secret_retrieve_api(
+    auth_user_api_client: APIClient,
+    secret_factory: SecretFactoryCallable,
+) -> None:
+    """Test secret retrieve API."""
+    my_secret = "ReadnBurn"
+    secret = secret_factory(plain_content=my_secret)
+    secret.refresh_from_db()
+
+    assert Secret.objects.get(id=secret.id).view_count == 0
+
+    response = auth_user_api_client.get(f"/api/secrets/retrieve/{secret.id}")
+
+    assert response.status_code == 200
+    response_json = response.json()
+
+    assert response_json["status"] == "success"
+    assert response_json["message"] == "Secret retrieved successfully"
+    assert response_json["data"]["content"] == my_secret
+
+    assert Secret.objects.get(id=secret.id).view_count == 1
+    assert SecretViewLog.objects.filter(secret=secret).all().count() == 1
+
+
+@pytest.mark.django_db
+def test_secret_retrieve_api_secret_does_not_exist(
+    auth_user_api_client: APIClient,
+) -> None:
+    """Test secret retrieve API with secret does not exist."""
+    response = auth_user_api_client.get(f"/api/secrets/retrieve/{uuid.uuid4()}")
+
+    assert response.status_code == 404
+    response_json = response.json()
+
+    assert response_json["status"] == "error"
+    assert response_json["message"] == "Secret not found"
+
+
+@pytest.mark.django_db
+@freeze_time("2025-01-01 00:00:00")
+def test_secret_retrieve_api_secret_expired(
+    auth_user_api_client: APIClient,
+    secret_factory: SecretFactoryCallable,
+) -> None:
+    """Test secret retrieve API with secret expired."""
+    my_secret = "ReadnBurn"
+    secret = secret_factory(
+        plain_content=my_secret, expires_at=timezone.now() - timedelta(days=1)
+    )
+    secret.refresh_from_db()
+
+    assert Secret.objects.get(id=secret.id).view_count == 0
+
+    with freeze_time("2025-01-03 00:00:00"):
+        response = auth_user_api_client.get(f"/api/secrets/retrieve/{secret.id}")
+
+        assert response.status_code == 410
+        response_json = response.json()
+
+        assert response_json["status"] == "error"
+        assert response_json["message"] == "Secret has expired"
+
+
+@pytest.mark.django_db
+def test_secret_retrieve_api_secret_already_viewed(
+    auth_user_api_client: APIClient,
+    secret_factory: SecretFactoryCallable,
+) -> None:
+    """Test secret retrieve API with secret already viewed. (With calling twice)"""
+    my_secret = "ReadnBurn"
+    secret = secret_factory(plain_content=my_secret)
+    secret.refresh_from_db()
+
+    assert Secret.objects.get(id=secret.id).view_count == 0
+
+    response_1 = auth_user_api_client.get(f"/api/secrets/retrieve/{secret.id}")
+
+    assert response_1.status_code == 200
+    response_json1 = response_1.json()
+
+    assert response_json1["status"] == "success"
+    assert response_json1["message"] == "Secret retrieved successfully"
+    assert response_json1["data"]["content"] == my_secret
+
+    response_2 = auth_user_api_client.get(f"/api/secrets/retrieve/{secret.id}")
+
+    assert response_2.status_code == 410
+    response_json2 = response_2.json()
+
+    assert response_json2["status"] == "error"
+    assert response_json2["message"] == "Secret has already been viewed or deleted"
+
+    # Because we are calling twice, it should be 2
+    assert SecretViewLog.objects.filter(secret=secret).all().count() == 2
+
+
+@pytest.mark.django_db
+def test_secret_retrieve_api_secret_deleted(
+    auth_user_api_client: APIClient,
+    secret_factory: SecretFactoryCallable,
+) -> None:
+    """Test secret retrieve API with secret deleted."""
+    my_secret = "ReadnBurn"
+    secret = secret_factory(plain_content=my_secret)
+    secret.refresh_from_db()
+
+    secret.soft_delete()  # Soft delete the secret
+    secret.refresh_from_db()
+
+    assert secret.is_deleted
+    assert secret.deleted_at is not None
+
+    response = auth_user_api_client.get(f"/api/secrets/retrieve/{secret.id}")
+
+    assert response.status_code == 410
+    response_json = response.json()
+
+    assert response_json["status"] == "error"
+    assert response_json["message"] == "Secret has already been viewed or deleted"
