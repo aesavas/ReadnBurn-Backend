8cbaf1edd2bfe6586fe65fc9538223c632c20f29
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-10-31 13:09:09 +0000

    feat: New custom exceptions created

--- backend/confidential/exceptions.py
+++ backend/confidential/exceptions.py
@@ -2,3 +2,27 @@ class SecretNotAvailableError(Exception):
     """Raised when an operation is attempted on an unavailable secret."""
 
     pass
+
+
+class SecretAlreadyViewedError(SecretNotAvailableError):
+    """Raised when an operation is attempted on an already viewed secret."""
+
+    pass
+
+
+class SecretAlreadyDeletedError(SecretNotAvailableError):
+    """Raised when an operation is attempted on an already deleted secret."""
+
+    pass
+
+
+class SecretDoesNotExistError(SecretNotAvailableError):
+    """Raised when an operation is attempted on a non-existent secret."""
+
+    pass
+
+
+class SecretExpiredError(SecretNotAvailableError):
+    """Raised when an operation is attempted on an expired secret."""
+
+    pass
