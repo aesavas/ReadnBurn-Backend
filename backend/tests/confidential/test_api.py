690ab96c4c36b8154fb83937cb328a82de7381b3
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-11-01 01:01:38 +0000

    test: unnecessary monkeypatch.setattr statements removed

--- backend/tests/confidential/test_api.py
+++ backend/tests/confidential/test_api.py
@@ -6,14 +6,11 @@ from typing import cast
 import pytest
 from confidential.models import Secret
 from confidential.models import SecretViewLog
-from core.encryption import decrypt_message
 from django.utils import timezone
 from freezegun import freeze_time
 from pytest import MonkeyPatch
 from rest_framework.test import APIClient
 from tests.conftest import SecretFactoryCallable
-
-from backend.settings_test import TEST_ENCRYPTION_KEY
 
 
 @pytest.mark.django_db
@@ -21,8 +18,6 @@ def test_secret_create_api(
     auth_user_api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "content": "Secret content",
         "expires_at": timezone.now() + timedelta(days=1),
@@ -35,13 +30,6 @@ def test_secret_create_api(
     assert response.status_code == 201
     assert response_json["status"] == "success"
     assert response_json["message"] == "Secret created successfully"
-
-    assert (
-        decrypt_message(
-            response_json["data"]["encrypted_content"], TEST_ENCRYPTION_KEY.encode()
-        )
-        == payload["content"]
-    )
     assert response_json["data"]["expires_at"] == cast(
         datetime, payload["expires_at"]
     ).isoformat().replace("+00:00", "Z")
@@ -53,8 +41,6 @@ def test_secret_create_api_invalid_content(
     auth_user_api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API with invalid content."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "content": "",
         "expires_at": timezone.now() + timedelta(days=1),
@@ -75,8 +61,6 @@ def test_secret_create_api_missing_content(
     auth_user_api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API with missing content."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "expires_at": timezone.now() + timedelta(days=1),
         "max_views": 10,
@@ -96,8 +80,6 @@ def test_secret_create_api_invalid_expires_at(
     auth_user_api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API with invalid expires at."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "content": "Secret content",
         "expires_at": timezone.now() - timedelta(days=1),
@@ -118,8 +100,6 @@ def test_secret_create_api_missing_max_views(
     auth_user_api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API with missing max views."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "content": "Secret content",
         "expires_at": timezone.now() + timedelta(days=1),
@@ -139,8 +119,6 @@ def test_secret_create_api_unauthorized(
     api_client: APIClient, monkeypatch: MonkeyPatch
 ) -> None:
     """Test secret creation API with unauthorized user."""
-    # Use test encryption key for this test
-    monkeypatch.setattr("backend.settings.ENCRYPTION_KEY", TEST_ENCRYPTION_KEY)
     payload = {
         "content": "Secret content",
         "expires_at": timezone.now() + timedelta(days=1),
