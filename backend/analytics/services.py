c60d54b56411bc79bab8399f9823ec9935143415
Ali Emre SAVAS <aliemresavas.95@gmail.com>
2025-11-03 21:18:51 +0000

    feat: caching added for statistics of users

--- backend/analytics/services.py
+++ backend/analytics/services.py
@@ -1,7 +1,9 @@
 from typing import Dict
+from typing import cast
 
 from accounts.models import User
 from confidential.models import Secret
+from django.core.cache import cache
 from django.db.models import F
 from django.db.models import Q
 from django.utils import timezone
@@ -13,22 +15,27 @@ class AnalyticsService:
     @staticmethod
     def get_user_stats(user: User) -> Dict[str, int]:
         """Get user stats."""
-        secrets = Secret.objects.filter(creator=user)
-        return {
-            "total_secrets": secrets.count(),
-            "active_secrets": secrets.filter(
-                Q(expires_at__gt=timezone.now())
-                & Q(view_count__lt=F("max_views"))
-                & Q(is_deleted=False)
-            ).count(),
-            "viewed_secrets": secrets.filter(view_count__gt=0).count(),
-            "pending_secrets": secrets.filter(
-                Q(view_count=0) & Q(expires_at__gt=timezone.now()) & Q(is_deleted=False)
-            ).count(),
-            "expired_secrets": secrets.filter(
-                Q(expires_at__lt=timezone.now()) & Q(is_deleted=False)
-            ).count(),
-            "never_viewed_secrets": secrets.filter(
-                Q(view_count=0) & Q(expires_at__lt=timezone.now())
-            ).count(),
-        }
+        if not (data := cache.get(f"user_stats_{user.id}")):
+            secrets = Secret.objects.filter(creator=user)
+            data = {
+                "total_secrets": secrets.count(),
+                "active_secrets": secrets.filter(
+                    Q(expires_at__gt=timezone.now())
+                    & Q(view_count__lt=F("max_views"))
+                    & Q(is_deleted=False)
+                ).count(),
+                "viewed_secrets": secrets.filter(view_count__gt=0).count(),
+                "pending_secrets": secrets.filter(
+                    Q(view_count=0)
+                    & Q(expires_at__gt=timezone.now())
+                    & Q(is_deleted=False)
+                ).count(),
+                "expired_secrets": secrets.filter(
+                    Q(expires_at__lt=timezone.now()) & Q(is_deleted=False)
+                ).count(),
+                "never_viewed_secrets": secrets.filter(
+                    Q(view_count=0) & Q(expires_at__lt=timezone.now())
+                ).count(),
+            }
+            cache.set(f"user_stats_{user.id}", data, timeout=60 * 5)
+        return cast(Dict[str, int], data)
